d3c953c7009acc622a8d11c85c7b5cfafd85a516
Hyuck9 <lhg1304@gmail.com>
2018-05-24 21:19:40 +0900

    Chat data class 구조 변경 Firebase Database에 Chat 관련 데이터 연동

--- app/src/main/java/me/hyuck9/hyucktalk/model/Chat.kt
+++ app/src/main/java/me/hyuck9/hyucktalk/model/Chat.kt
@@ -1,6 +1,11 @@
 package me.hyuck9.hyucktalk.model
 
 data class Chat(
-        var uid: String? = null,
-        var destinationUid: String? = null
-)
+        var users: HashMap<String, Boolean> = HashMap(),
+        var comments: HashMap<String, Comment> = HashMap()) {
+
+    companion object {
+        data class Comment(val uid:String? = null, val message: String? = null)
+    }
+
+}
--- app/src/main/java/me/hyuck9/hyucktalk/ui/activity/MessageActivity.kt
+++ app/src/main/java/me/hyuck9/hyucktalk/ui/activity/MessageActivity.kt
@@ -3,27 +3,67 @@ package me.hyuck9.hyucktalk.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_message.*
 import me.hyuck9.hyucktalk.R
 import me.hyuck9.hyucktalk.model.Chat
 
 class MessageActivity : AppCompatActivity() {
 
+    private var uid: String? = null
+    private var destinationUid: String? = null
+    private var chatRoomUid: String? = null
+    private val chatRoomRef = FirebaseDatabase.getInstance().getReference("chat_rooms")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
 
-        val destinationUid = intent.getStringExtra("destinationUid")
+        uid = FirebaseAuth.getInstance().currentUser!!.uid
+        destinationUid = intent.getStringExtra("destinationUid")
 
-        message_a_btn_send.setOnClickListener {
-            val chat = Chat(
-                    FirebaseAuth.getInstance().currentUser!!.uid,
-                    destinationUid
-            )
+        message_a_btn_send.setOnClickListener { sendButtonClicked() }
+        checkChatRoom()
 
-            FirebaseDatabase.getInstance().getReference("chat_rooms").push().setValue(chat)
+    }
 
+    private fun sendButtonClicked() {
+        val chat = Chat().apply {
+            users[uid!!] = true
+            users[destinationUid!!] = true
         }
+
+        if (chatRoomUid.isNullOrBlank()) {
+            chatRoomRef.push().setValue(chat)
+        } else {
+            val comment = Chat.Companion.Comment(uid, message_a_et_input.text.toString())
+            chatRoomRef.child(chatRoomUid).child("comments").push().setValue(comment)
+        }
+
+        checkChatRoom()
+    }
+
+    private fun checkChatRoom() {
+        chatRoomRef.orderByChild("users/$uid").equalTo(true).addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot?) {
+
+                dataSnapshot!!.children.forEach {
+                    val chat = it.getValue(Chat::class.java)
+                    if ( chat!!.users.containsKey(destinationUid) ) {
+                        chatRoomUid = it.key
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 }
